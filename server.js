e2edaa6681739eefa01d1b94883806a81a63ad97
Sushil <being.sushil9@gmail.com>
2021-06-12 19:03:35 +0530

    allowed cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
-const corsOptions = {
-	origin: "*",
-};
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
